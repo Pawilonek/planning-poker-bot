44283f17463e354c2a56affe948be4c1f5fdbd83
Paweł Kamiński <pawel13k@gmail.com>
2023-03-20 20:52:54 +0100

    feat: Add testing http server

--- cmd/scrumpoke/main.go
+++ cmd/scrumpoke/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/Pawilonek/scrumpoke/internal/bots"
 )
 
+func testHttp(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "This is my website!")
+}
+
 func main() {
     fmt.Println(bots.TestMessage())
+
+	http.HandleFunc("/", testHttp)
+
+	err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        panic(err)
+    }
 }
 
